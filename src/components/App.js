2b17ac2bd0618985a09bc54a01d188c9767abcbe
Nightrider0098 <hitesh08011999@gmail.com>
2020-11-16 11:04:18 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState, useEffect } from "react";
 import '../styles/App.css';
 
 const App = () => {
+  let timerId = ""
+  let [timeLeft, setReverseCount] = useState(0);
+
+  // write your code here 
+
   return (
-    <div id="main"></div>
+    <div className="wrapper">
+      <div id="whole-center"><h1>Count in Reverse for<input id="timeCount" onKeyDown={countInReverse} /> sec. </h1>
+      </div>
+      <div id="current-time">{timeLeft}</div>
+    </div>
   )
 }
 
