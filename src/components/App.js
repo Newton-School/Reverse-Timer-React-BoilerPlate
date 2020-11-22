0b9a89799e7f80ae18bc0762cc6de6d02305feab
Shreya Chandra <shreya.chandra96@gmail.com>
2020-11-23 01:02:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import '../styles/App.css';
 
 const App = () => {
   let timerId = ""
-  let [timeLeft, setReverseCount] = useState(0);
+  let [timeLeft, setTimeLeft] = useState(0);
 
   // write your code here 
 
   return (
     <div className="wrapper">
-      <div id="whole-center"><h1>Count in Reverse for<input id="timeCount" onKeyDown={countInReverse} /> sec. </h1>
+      <div id="whole-center">
+        <h1>
+          Reverse countdown for<input id="timeCount" onKeyDown={countInReverse} /> sec.
+        </h1>
       </div>
       <div id="current-time">{timeLeft}</div>
     </div>
